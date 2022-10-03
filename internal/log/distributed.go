4a2ce2330357c3bc7cca84a4316fdec396f626df
tommy <kazu9su@gmail.com>
2022-10-04 08:25:27 +0900

    update

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -2,7 +2,10 @@ package log
 
 import (
 	"bytes"
+	"crypto/tls"
+	"fmt"
 	"io"
+	"net"
 	"os"
 	"path/filepath"
 	"time"
@@ -10,13 +13,13 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/hashicorp/raft"
 	raftboltdb "github.com/hashicorp/raft-boltdb"
-	"google.golang.org/grpc/internal/transport"
+	api "github.com/kazu9su/proglog/api/v1"
 )
 
 type DistributedLog struct {
 	config Config
-	log *Log
-	raft *raft.Raft
+	log    *Log
+	raft   *raft.Raft
 }
 
 func NewDistributedLog(dataDir string, config Config) (
@@ -46,7 +49,7 @@ func (l *DistributedLog) setupLog(dataDir string) error {
 }
 
 func (l *DistributedLog) setupRaft(dataDir string) error {
-	fsm := &fsm{logz: l.log}
+	fsm := &fsm{log: l.log}
 	logDir := filepath.Join(dataDir, "raft", "log")
 	if err := os.MkdirAll(logDir, 0755); err != nil {
 		return err
@@ -72,7 +75,7 @@ func (l *DistributedLog) setupRaft(dataDir string) error {
 	if err != nil {
 		return err
 	}
-	
+
 	maxPool := 5
 	timeout := 10 * time.Second
 	transport := raft.NewNetworkTransport(
@@ -85,7 +88,7 @@ func (l *DistributedLog) setupRaft(dataDir string) error {
 	config := raft.DefaultConfig()
 	config.LocalID = l.config.Raft.LocalID
 	if l.config.Raft.HeartbeatTimeout != 0 {
-		config.HeartBeatTimeout = l.config.Raft.HeartBeatTimeout
+		config.HeartbeatTimeout = l.config.Raft.HeartbeatTimeout
 	}
 	if l.config.Raft.ElectionTimeout != 0 {
 		config.ElectionTimeout = l.config.Raft.ElectionTimeout
@@ -94,13 +97,14 @@ func (l *DistributedLog) setupRaft(dataDir string) error {
 		config.LeaderLeaseTimeout = l.config.Raft.LeaderLeaseTimeout
 	}
 	if l.config.Raft.CommitTimeout != 0 {
-		config.CommitTimeout = l.config.CommitTimeout
+		config.CommitTimeout = l.config.Raft.CommitTimeout
 	}
 
 	l.raft, err = raft.NewRaft(
 		config,
 		fsm,
 		logStore,
+		stableStore,
 		snapshotStore,
 		transport,
 	)
@@ -118,7 +122,7 @@ func (l *DistributedLog) setupRaft(dataDir string) error {
 	if l.config.Raft.Bootstrap && !hasState {
 		config := raft.Configuration{
 			Servers: []raft.Server{{
-				ID: config.LocalID,
+				ID:      config.LocalID,
 				Address: transport.LocalAddr(),
 			}},
 		}
@@ -167,18 +171,77 @@ func (l *DistributedLog) apply(reqType RequestType, req proto.Message) (
 	return res, nil
 }
 
-func (l *DistributedLog) Read(offset uint64) (*api.Record, error) [
-	reutnr l.log.Read(offset)
-]
+func (l *DistributedLog) Read(offset uint64) (*api.Record, error) {
+	return l.log.Read(offset)
+}
+
+func (l *DistributedLog) Join(id, addr string) error {
+	configFuture := l.raft.GetConfiguration()
+	if err := configFuture.Error(); err != nil {
+		return err
+	}
+	serverID := raft.ServerID(id)
+	serverAddr := raft.ServerAddress(addr)
+	for _, srv := range configFuture.Configuration().Servers {
+		if srv.ID == serverID || srv.Address == serverAddr {
+			if srv.ID == serverID && srv.Address == serverAddr {
+				// server has already joined
+				return nil
+			}
+			// remove the existing server
+			removeFuture := l.raft.RemoveServer(serverID, 0, 0)
+			if err := removeFuture.Error(); err != nil {
+				return err
+			}
+		}
+	}
+	addFuture := l.raft.AddVoter(serverID, serverAddr, 0, 0)
+	if err := addFuture.Error(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (l *DistributedLog) Leave(id string) error {
+	removeFuture := l.raft.RemoveServer(raft.ServerID(id), 0, 0)
+	return removeFuture.Error()
+}
+
+func (l *DistributedLog) WaitForLeader(timeout time.Duration) error {
+	timeoutc := time.After(timeout)
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-timeoutc:
+			return fmt.Errorf("timed out")
+		case <-ticker.C:
+			if l := l.raft.Leader(); l != "" {
+				return nil
+			}
+		}
+	}
+}
+
+func (l *DistributedLog) Close() error {
+	f := l.raft.Shutdown()
+	if err := f.Error(); err != nil {
+		return err
+	}
+	return l.log.Close()
+}
 
 var _ raft.FSM = (*fsm)(nil)
+
 type fsm struct {
-	log Log
+	log *Log
 }
 type RequestType uint8
+
 const (
 	AppendRequestType RequestType = 0
 )
+
 func (l *fsm) Apply(record *raft.Log) interface{} {
 	buf := record.Data
 	reqType := RequestType(buf[0])
@@ -199,7 +262,7 @@ func (l *fsm) applyAppend(b []byte) interface{} {
 	if err != nil {
 		return err
 	}
-	return &api.Produe{Offset: offset}
+	return &api.ProduceResponse{Offset: offset}
 }
 
 func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
@@ -226,7 +289,7 @@ func (s *snapshot) Release() {}
 func (f *fsm) Restore(r io.ReadCloser) error {
 	b := make([]byte, lenWidth)
 	var buf bytes.Buffer
-	for i := 0; i++ {
+	for i := 0; ; i++ {
 		_, err := io.ReadFull(r, b)
 		if err == io.EOF {
 			break
@@ -255,7 +318,8 @@ func (f *fsm) Restore(r io.ReadCloser) error {
 	return nil
 }
 
-var _ raft.logStore = (*logStore)(nil)
+var _ raft.LogStore = (*logStore)(nil)
+
 type logStore struct {
 	*Log
 }
@@ -296,8 +360,8 @@ func (l *logStore) StoreLogs(records []*raft.Log) error {
 	for _, record := range records {
 		if _, err := l.Append(&api.Record{
 			Value: record.Data,
-			Term: record.Term,
-			Type: uint32(record.Type),
+			Term:  record.Term,
+			Type:  uint32(record.Type),
 		}); err != nil {
 			return err
 		}
@@ -308,4 +372,73 @@ func (l *logStore) StoreLogs(records []*raft.Log) error {
 
 func (l *logStore) DeleteRange(min, max uint64) error {
 	return l.Truncate(max)
+}
+
+var _ raft.StreamLayer = (*StreamLayer)(nil)
+
+type StreamLayer struct {
+	ln              net.Listener
+	serverTLSConfig *tls.Config
+	peerTLSConfig   *tls.Config
+}
+
+func NewStreamLayer(
+	ln net.Listener,
+	serverTLSConfig,
+	peerTLSConfig *tls.Config,
+) *StreamLayer {
+	return &StreamLayer{
+		ln:              ln,
+		serverTLSConfig: serverTLSConfig,
+		peerTLSConfig:   peerTLSConfig,
+	}
+}
+
+const RaftRPC = 1
+
+func (s *StreamLayer) Dial(
+	addr raft.ServerAddress,
+	timeout time.Duration,
+) (net.Conn, error) {
+	dialer := &net.Dialer{Timeout: timeout}
+	var conn, err = dialer.Dial("tcp", string(addr))
+	if err != nil {
+		return nil, err
+	}
+	// identify to mux this is a raft rpc
+	_, err = conn.Write([]byte{byte(RaftRPC)})
+	if err != nil {
+		return nil, err
+	}
+	if s.peerTLSConfig != nil {
+		conn = tls.Client(conn, s.peerTLSConfig)
+	}
+	return conn, err
+}
+
+func (s *StreamLayer) Accept() (net.Conn, error) {
+	conn, err := s.ln.Accept()
+	if err != nil {
+		return nil, err
+	}
+	b := make([]byte, 1)
+	_, err = conn.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	if bytes.Compare([]byte{byte(RaftRPC)}, b) != 0 {
+		return nil, fmt.Errorf("not a raft rpc")
+	}
+	if s.serverTLSConfig != nil {
+		return tls.Server(conn, s.serverTLSConfig), nil
+	}
+	return conn, nil
+}
+
+func (s *StreamLayer) Close() error {
+	return s.ln.Close()
+}
+
+func (s *StreamLayer) Addr() net.Addr {
+	return s.ln.Addr()
 }
---
+++ internal/log/distributed_test.go
@@ -0,0 +1,61 @@
+package log_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/raft"
+	"github.com/kazu9su/proglog/internal/log"
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+)
+
+func TestMultipleNodes(t *testing.T) {
+	var logs []*log.DistributedLog
+	nodeCount := 3
+	ports := dynaport.Get(nodeCount)
+
+	for i := 0; i < nodeCount; i++ {
+		dataDir, err := ioutil.TempDir("", "distributed-log-test")
+		require.NoError(t, err)
+		defer func(dir string) {
+			_ = os.RemoveAll(dir)
+		}(dataDir)
+
+		ln, err := net.Listen(
+			"tcp",
+			fmt.Sprintf("127.0.0.1:%d", ports[i]),
+		)
+		require.NoError(t, err)
+
+		config := log.Config{}
+		config.Raft.StreamLayer = log.NewStreamLayer(ln, nil, nil)
+		config.Raft.LocalID = raft.ServerID(fmt.Sprintf("%d", i))
+		config.Raft.HeartbeatTimeout = 50 * time.Millisecond
+		config.Raft.ElectionTimeout = 50 * time.Millisecond
+		config.Raft.LeaderLeaseTimeout = 50 * time.Millisecond
+		config.Raft.CommitTimeout = 5 * time.Millisecond
+
+		if i == 0 {
+			config.Raft.Bootstrap = true
+		}
+		l, err := log.NewDistributedLog(dataDir, config)
+		require.NoError(t, err)
+
+		if i != 0 {
+			err = logs[0].Join(
+				fmt.Sprintf("%d", i), ln.Addr().String(),
+			)
+
+			require.NoError(t, err)
+		} else {
+			err = l.WaitForLeader(3 * time.Second)
+			require.NoError(t, err)
+		}
+		logs = append(logs, l)
+	}
+}
